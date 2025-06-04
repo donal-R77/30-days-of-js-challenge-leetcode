18229c6c4cbfd875c4c80588c500bfa4742d5246
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-04 12:04:04 +0300

    first test

--- 04-counter-II/index.js
+++ 04-counter-II/index.js
@@ -3,8 +3,25 @@
  * @return { increment: Function, decrement: Function, reset: Function }
  */
 var createCounter = function(init) {
-    
+    let valIncrement = init;
+    let valDecrement = init;
+    let resetValue = init;
+    let increment = function() {
+            return ++valIncrement;
+        }
+    let decrement = function() {
+            return --valDecrement;
+        }
+    let reset = function() {
+        return resetValue;
+    }
+    return {increment, decrement, reset};
 };
+const counter = createCounter(5);
+console.log(counter.increment());
+console.log(counter.decrement());
+console.log(counter.reset());
+
 
 /**
  * const counter = createCounter(5)
