10c96bfd77440a3d31933725c9b777dad93ed87e
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-05 19:44:28 +0300

    Test Done

--- Apply 05-Transform Over Each Element in Array/index.js
+++ Apply 05-Transform Over Each Element in Array/index.js
@@ -3,16 +3,13 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    let returnedArray = [];
-    for (let i = 0; i< arr.length; i++) { 
-        returnedArray[i] = fn(arr[i],i);
-   
-    }
-    return returnedArray;
+var map = function (arr, fn) {
+  let returnedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    returnedArray[i] = fn(arr[i], i);
+  }
+  return returnedArray;
 };
-function somme(n,i) {
- return n + i;    
-}
-const newArray = map([1,2,3,4],somme);
+
+const newArray = map([1, 2, 3, 4], somme);
 console.log(newArray);
