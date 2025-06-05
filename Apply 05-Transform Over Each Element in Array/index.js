4e8753939f261eb2df5c9c8b5a7b1990e017e0b4
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-05 19:25:02 +0300

    first test of algo

--- Apply 05-Transform Over Each Element in Array/index.js
+++ Apply 05-Transform Over Each Element in Array/index.js
@@ -4,5 +4,15 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    
+    let returnedArray = [];
+    for (let i = 0; i< arr.length; i++) { 
+        returnedArray[i] = fn(arr[i],i);
+   
+    }
+    return returnedArray;
 };
+function somme(n,i) {
+ return n + i;    
+}
+const newArray = map([1,2,3,4],somme);
+console.log(newArray);
