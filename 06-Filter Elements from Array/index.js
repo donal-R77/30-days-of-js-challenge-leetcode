6bdd1070e5564ed79c97b084d267af0abed2435a
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-06 23:04:42 +0300

    Algo done

--- 06-Filter Elements from Array/index.js
+++ 06-Filter Elements from Array/index.js
@@ -4,5 +4,15 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    
+    let filteredArr = [];
+    let j = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i],i)) {
+            filteredArr.push(arr[i])
+        }
+    }
+    return filteredArr;       
 };
+arr = [-2,-1,0,1,2], fn = function greaterThan10(n) { return n  + 1; }
+const newArray = filter(arr, fn);
+console.log(newArray);
