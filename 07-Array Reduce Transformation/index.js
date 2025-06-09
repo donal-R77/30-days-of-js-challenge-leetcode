a30b749e135159c24dde328565ccd0eeb1fa8b0a
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-09 23:09:01 +0300

    Algo 7 done

--- 07-Array Reduce Transformation/index.js
+++ 07-Array Reduce Transformation/index.js
@@ -5,5 +5,20 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    
+    let val = init;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.length === 0) {
+             val = init;
+        }
+        val = fn(val,nums[i]);
+        
+      
+    }
+    return val;
 };
+
+nums = [1,2,3,4];
+fn = function sum(accum, curr) { return accum + curr*curr; };
+init = 100;
+const newArray = reduce(nums,fn,init);
+console.log(newArray);
