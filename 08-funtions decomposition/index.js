20d15ab6cee1951370b6890ff1c62d584345321d
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-10 20:22:24 +0300

    function composition done

--- 08-funtions decomposition/index.js
+++ 08-funtions decomposition/index.js
@@ -5,10 +5,14 @@
 var compose = function(functions) {
     
     return function(x) {
-        
+        if (functions == []) {
+            return [...functions] = x;
+        }
+        return  [...functions].reduceRight((acc,fn) => fn(acc),x);
     }
 };
-
+const fn = compose([x => x + 1, x => 2 * x]);
+console.log(fn(4));
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
