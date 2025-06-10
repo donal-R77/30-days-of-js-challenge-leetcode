ecdd199c9471d7ad68622b98d6c1588fc604b4c2
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-10 20:48:32 +0300

    use the ternary condition

--- 08-funtions composition/index.js
+++ 08-funtions composition/index.js
@@ -4,11 +4,8 @@
  */
 var compose = function(functions) {
     
-    return function(x) {
-        if (functions == []) {
-            return [...functions] = x;
-        }
-        return  [...functions].reduceRight((acc,fn) => fn(acc),x);
+    return function(x) {      
+        return (functions.length === 0)?x:[...functions].reduceRight((acc,fn) => fn(acc),x);
     }
 };
 const fn = compose([x => x + 1, x => 2 * x]);
