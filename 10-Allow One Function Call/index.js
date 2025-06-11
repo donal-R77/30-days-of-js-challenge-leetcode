a06a124cfbaa036808a02c3344cc3609740c3bfc
donal-R77 <rakotoarisoaromarick@gmail.com>
2025-06-11 22:09:19 +0300

    problem resolvzd

--- 10-Allow One Function Call/index.js
+++ 10-Allow One Function Call/index.js
@@ -2,9 +2,18 @@
  * @param {Function} fn
  * @return {Function}
  */
-var once = function(fn) {
-    
-    return function(...args){
-        
+var once = function (fn) {
+  let calls = 0;
+  return function (...args) {
+    if (calls++ < 1) {
+      return fn(...args);
+    } else {
+      return undefined;
     }
+  };
 };
+fn = (a, b, c) => a * b * c;
+const onceFn = once(fn);
+console.log(`${onceFn(5, 7, 4)}`);
+console.log(`${onceFn(2, 3, 6)}`);
+console.log(`${onceFn(4, 6, 8)}`);
